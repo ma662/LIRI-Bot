24b46d20a781a3bd15a82c133abb75ec4f467857
Chlora <ma662@njit.edu>
2019-02-09 09:42:35 -0500

    omdb, spotify calls functional

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = { 
+    key: process.env.OMDB_KEY
+}
---
+++ liri.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+var Spotify = require('node-spotify-api');
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+
+// C O M M A N D S
+// * `concert-this`
+// * `spotify-this-song`
+// * `movie-this`
+// * `do-what-it-says`
+
+// WORKING SPOTIFY 
+// var spotify = new Spotify({
+    //   id: <your spotify client id>,
+    //   secret: <your spotify client secret>
+    // });
+    
+    // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    //     if (err) {
+    //         return console.log('Error occurred: ' + err);
+    //     }
+        
+    //     console.log(data); 
+    // });
+    
+
+var axios = require("axios");
+// We then run the request with axios module on a URL with a JSON
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    // Then we print out the imdbRating
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
+
+
+
+
+
+
